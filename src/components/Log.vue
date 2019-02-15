5f82968867656acd0f9bff03caa015696d4fd64d
Tang R <lgctangrong@163.com>
2019-02-15 13:55:41 +0800

    new

---
+++ src/components/Log.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+   <h1>欢迎来日志</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Log'
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/User.vue
@@ -0,0 +1,19 @@
+<template>
+ <div id="user">
+   <h1>欢迎来到用户列表 {{username}}</h1>
+ </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  name: 'User',
+  data () {
+    return {
+      username: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
