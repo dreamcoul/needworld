b49bc79d68d68e08170aa03b81dd9a3ffadaa506
Tang R <lgctangrong@163.com>
2019-02-15 16:30:34 +0800

    0.2

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Log from './../components/Log.vue'
+import User from '@/components/User'
+// import Index from '@/page/index'
+// import Content from '@/page/content'
 
 Vue.use(Router)
 
@@ -16,6 +19,11 @@ export default new Router({
       path: '/log',
       name: 'Log',
       component: Log
+    },
+    {
+      path: '/user',
+      name: 'User',
+      component: User
     }
   ]
 })
